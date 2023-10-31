7b21b41aaa137e3f827fdfba7e9ade427f60e9db
Muhammed Babacan <mbbcn05@gmail.com>
2023-10-31 14:07:53 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.babacan05.ewggame"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 4
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
